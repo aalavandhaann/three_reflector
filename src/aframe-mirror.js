aecdf86a63dc93b1fda78be7c0d94f733af1bcb6
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2018-03-07 14:53:45 -0500

    Commit first push of source code with examples

---
+++ src/aframe-mirror.js
@@ -0,0 +1,43 @@
+AFRAME.registerComponent('aframe-mirror', 
+{
+	schema:{
+	    textureOne: {default: undefined},
+	    textureTwo: {default: undefined},
+	    wrapOne: 
+	    {
+	    	type: 'vec2',
+	    	default: {x: 1, y: 1}
+	    },
+	    wrapTwo: 
+	    {
+	    	type: 'vec2',
+	    	default: {x: 1, y: 1}
+	    },
+	    invertedUV:
+	    {
+	    	type: 'bool',
+	    	default: false
+	    },
+	    textureWidth: {default: 512},
+	    textureHeight: {default: 512},
+	    color: {default: new THREE.Color(0x7F7F7F)},
+	    intensity: {default: 1.0},
+	},
+	init: function () 
+	{
+	    var scene = this.el.sceneEl;
+	    var three_scene = scene.object3D;
+	    var mirrorObj = this.el.getObject3D('mesh');
+
+	    if(!mirrorObj)
+	    {
+	    	return;
+	    }
+
+	    var gscenereflector = Ashok.GroundSceneReflector(mirrorObj, scene.renderer, three_scene, this.data);
+	},
+	tick: function () 
+	{	    	
+
+	}
+});
